27b29b103019aacb36ae915e8c43f27d64d65ca9
tao.chen <tao.chen@bnsf.com>
2022-09-18 16:15:23 -0400

    create fenzhi 01

--- review/MyException.java
+++ review/MyException.java
@@ -3,8 +3,8 @@ package com.tao.bintree.tao1;
 public class MyException extends Exception{
     private int id ;
     public MyException(String message, int id){
-        super(message);
-        this.id = id ;
+        super(message);// create fenzhi01
+        this.id = id ;//test
     }
 
     public int getId() {
